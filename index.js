63ef398d0f80a656db28e52f05775485179a0dcd
shirley <chefmadalion@gmail.com>
2025-11-21 20:18:30 +0300

    added index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+let banana=10;
+let cheese=2;
+
+if (banana=cheese) {
+     return true;
+  } else {
+    return false;
+};
+  
+const average = (num1, num2) => (num1 + num2) / 2;
+let result = average(90.5 ,5.5);
+console.log(result);
